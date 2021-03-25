bb7448476a0d8885290ad7fd5dd178852ef73400
Rafaelmos <rafael.rjms0@gmail.com>
2021-03-25 17:54:13 -0300

    geradorRGBA

--- miniprojetos/geradorRGBA/script.js
+++ miniprojetos/geradorRGBA/script.js
@@ -2,15 +2,27 @@ function gerarCor() {
     let red = document.getElementById("red").value
     let green = document.getElementById("green").value
     let blue = document.getElementById("blue").value
+    let alpha = document.getElementById("alpha").value
+    // let hexa = document.getElementById("hexa").value
 
-    let cor = `rgb(${red},${green},${blue},${10})`
+    let cor = `rgb(${red},${green},${blue},${alpha/10})`
 
     document.getElementById("cor").style.background = cor
+
+    // document.getElementById("hexa").innerHTML = `#${hexa}`
+    document.getElementById("r").innerHTML = `R: ${red}`
+    document.getElementById("g").innerHTML = `G: ${green}`
+    document.getElementById("b").innerHTML = `B: ${blue}`   
+    document.getElementById("a").innerHTML = `A: ${alpha/10}`
 
     document.getElementById("txtRed").innerHTML = `R: ${red}`
     document.getElementById("txtGreen").innerHTML = `G: ${green}`
     document.getElementById("txtBlue").innerHTML = `B: ${blue}`
+    document.getElementById("txtAlpha").innerHTML = `A: ${alpha*10}%`
+
  
 
     setTimeout("gerarCor()",1)
 }
+
+
